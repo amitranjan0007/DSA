8f2a4155ffd127bf64af2d0be6e6d73de207f22f
amitranjan <sr.amitranjan@gmail.com>
2025-01-06 23:45:17 +0530

    Follow up question maxNumberClusterSelectingFive

--- src/desiqna/MaxNumberOfCluster.java
+++ src/desiqna/MaxNumberOfCluster.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 
 public class MaxNumberOfCluster {
     public static void main(String[] args) {
-        maxNumberCluster();
+        //maxNumberCluster();
+        maxNumberClusterSelectingFive();
     }
     private static void maxNumberCluster(){
         /*
@@ -24,6 +25,31 @@ public class MaxNumberOfCluster {
         int count=0;
         int sum=0;
         for(int j=n-1;j>=1;j=j-2){
+            sum+=arr[j];
+            count++;
+            if(count==groupSize){
+                break;
+            }
+        }
+        System.out.println(sum);
+
+    }
+
+    private static void maxNumberClusterSelectingFive(){
+
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        int arr[]=new int[n+1];
+
+        for(int i=1;i<=n;i++){
+            arr[i]=scanner.nextInt();
+        }
+
+        Arrays.sort(arr);
+        int groupSize=n/5;
+        int count=0;
+        int sum=0;
+        for(int j=n-2;j>=1;j=j-3){
             sum+=arr[j];
             count++;
             if(count==groupSize){
