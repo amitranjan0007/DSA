56d0f22791fb26ce16ed64319811f01cfff487c2
amitranjan <sr.amitranjan@gmail.com>
2025-01-06 23:54:51 +0530

    Follow up question maxNumberClusterSelectingFive added observation

--- src/desiqna/MaxNumberOfCluster.java
+++ src/desiqna/MaxNumberOfCluster.java
@@ -10,7 +10,21 @@ public class MaxNumberOfCluster {
     }
     private static void maxNumberCluster(){
         /*
-        Observation: Divide the arr in 3 e
+        Observation:
+        Sort the array because max will go on right
+          - in group of 3 ,always remember you need second largest element,not worried about that 1st largest element
+            Just for observation [5 17 19] [20 29 40] ===> as you see we not worried about 19 & 20 we concerned about 17 & 29
+
+            eg:
+             5 17 19 20 29 40
+
+             size of array(n)=6
+             group we can make 6/3=2 group
+
+             How you get max median in 3 number of group
+             so we select 29 ==> [5,29,40]  ====> max median is 29
+              Again we select 19 ==> [17,19,20] ====> max median is 19
+              Add 29+119=48 ans
         * */
         Scanner scanner=new Scanner(System.in);
         int n=scanner.nextInt();
@@ -36,7 +50,8 @@ public class MaxNumberOfCluster {
     }
 
     private static void maxNumberClusterSelectingFive(){
-
+       //Follow up question of above ,same concept ,select keep select 3 element from back and 2 from front
+        //we don't care about front selection,because while iterating we count==groupsize or not
         Scanner scanner=new Scanner(System.in);
         int n=scanner.nextInt();
         int arr[]=new int[n+1];
