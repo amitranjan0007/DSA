6506fdecef07df61667293b2fcfadd71bc4135b9
amitranjan <sr.amitranjan@gmail.com>
2025-01-06 16:37:32 +0530

    added the understanding of problem

--- src/leetcode/dynamicprogramming/LISWithDecAdjacentDiff.java
+++ src/leetcode/dynamicprogramming/LISWithDecAdjacentDiff.java
@@ -4,10 +4,44 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class LISWithDecAdjacentDiff {
+    //Problem Link: https://leetcode.com/problems/longest-subsequence-with-decreasing-adjacent-difference/
     public static void main(String[] args) {
         longestSubsequence();
     }
     public static void longestSubsequence() {
+        /*
+        TC:300*N^2
+
+        Understanding:
+        Find longest sequence which is valid.
+
+            -> Any sequence which is valid will always have decreasing order of their adjacent differences.
+
+            -> [6 5 3 4 2 1]
+
+            -> [6,4,2,1] =>(2,2,1)
+
+            ->adj1 >= ad2 >= ……..
+
+            -> dp[i] = largest valid sequence ending at index “i” (ith element should be included forcefully)
+
+            -> not enough to solve.
+
+            -> increase the state.
+
+            -> dp[i][diff] = largest valid sequence ending at index “i” such that its difference with the second last element is diff.
+
+            -> whenever you are at index “i” -> travel from 0 to i-1 ;
+
+            -> let us say abs(b[j]-b[i]) == diff; j < i
+
+            -> dp[i][diff] = 1 + (dp[j][diff] OR dp[j][diff+1] OR                      dp[j][diff+2] OR
+                                = ……………………………….. OR dp[j][300]
+                              = 1 + max of all of them.
+
+            -> final answer = max(dp[1][from 0 to 300] , dp[2][...],.....dp[n][...])
+
+        * */
         Scanner scanner=new Scanner(System.in);
         int n=scanner.nextInt();
         int[]arr=new int[n+1];
