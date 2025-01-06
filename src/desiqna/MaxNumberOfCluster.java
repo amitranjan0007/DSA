0d4113549504a81405cb79c80fe7203c44bbbceb
amitranjan <sr.amitranjan@gmail.com>
2025-01-06 22:38:54 +0530

    max number cluster

---
+++ src/desiqna/MaxNumberOfCluster.java
@@ -0,0 +1,36 @@
+package desiqna;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MaxNumberOfCluster {
+    public static void main(String[] args) {
+        maxNumberCluster();
+    }
+    private static void maxNumberCluster(){
+        /*
+        Observation: Divide the arr in 3 e
+        * */
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        int arr[]=new int[n+1];
+
+        for(int i=1;i<=n;i++){
+            arr[i]=scanner.nextInt();
+        }
+
+        Arrays.sort(arr);
+        int groupSize=n/3;
+        int count=0;
+        int sum=0;
+        for(int j=n-1;j>=1;j=j-2){
+            sum+=arr[j];
+            count++;
+            if(count==groupSize){
+                break;
+            }
+        }
+        System.out.println(sum);
+
+    }
+}
